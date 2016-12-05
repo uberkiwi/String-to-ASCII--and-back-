421a2653d942fb7ec9bf70b5b3e4817eb8c2ac89
uberkiwi <uberkiwi125@googlemail.com>
2016-12-05 22:35:26 +0000

    y

---
+++ Write to text/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Encode = New System.Windows.Forms.Button()
+        Me.Entry = New System.Windows.Forms.TextBox()
+        Me.Decode = New System.Windows.Forms.Button()
+        Me.KeyBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Encode
+        '
+        Me.Encode.Location = New System.Drawing.Point(13, 64)
+        Me.Encode.Name = "Encode"
+        Me.Encode.Size = New System.Drawing.Size(124, 76)
+        Me.Encode.TabIndex = 0
+        Me.Encode.Text = "Encode"
+        Me.Encode.UseVisualStyleBackColor = True
+        '
+        'Entry
+        '
+        Me.Entry.Location = New System.Drawing.Point(12, 12)
+        Me.Entry.Name = "Entry"
+        Me.Entry.Size = New System.Drawing.Size(260, 20)
+        Me.Entry.TabIndex = 1
+        Me.Entry.Text = "Enter the message..."
+        '
+        'Decode
+        '
+        Me.Decode.Location = New System.Drawing.Point(148, 64)
+        Me.Decode.Name = "Decode"
+        Me.Decode.Size = New System.Drawing.Size(124, 76)
+        Me.Decode.TabIndex = 2
+        Me.Decode.Text = "Decode"
+        Me.Decode.UseVisualStyleBackColor = True
+        '
+        'KeyBox
+        '
+        Me.KeyBox.Location = New System.Drawing.Point(12, 38)
+        Me.KeyBox.Name = "KeyBox"
+        Me.KeyBox.Size = New System.Drawing.Size(257, 20)
+        Me.KeyBox.TabIndex = 3
+        Me.KeyBox.Text = "Key"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 149)
+        Me.Controls.Add(Me.KeyBox)
+        Me.Controls.Add(Me.Decode)
+        Me.Controls.Add(Me.Entry)
+        Me.Controls.Add(Me.Encode)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "EN/DECRYPT"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Encode As Button
+    Friend WithEvents Entry As TextBox
+    Friend WithEvents Decode As Button
+    Friend WithEvents KeyBox As TextBox
+End Class
---
+++ Write to text/Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles Entry.TextChanged
+
+    End Sub
+
+    Private Sub Encode_Click(sender As Object, e As EventArgs) Handles Encode.Click
+        Dim sText As String
+        Dim i As Integer
+        Dim lenText As Integer
+        Dim Msg As String
+
+        sText = Entry.Text
+        lenText = Len(sText)
+
+        For i = 1 To lenText
+            Msg = Msg & CStr(Asc(Mid$(sText, i, 1)))
+            Msg = Msg + " "
+        Next i
+        Dim fw As StreamWriter
+        If (Entry.Text = "" Or Entry.Text = "Please enter something.") Then
+            Entry.Text = "Please enter something."
+            Return
+        End If
+        Dim ReadString As String
+        Try
+            Dim s As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            fw = New StreamWriter(s & "\text.txt", True)
+            ReadString = Msg
+            fw.WriteLine(ReadString)
+        Finally
+            fw.Close()
+        End Try
+    End Sub
+
+    Private Sub Decode_Click(sender As Object, e As EventArgs) Handles Decode.Click
+        Dim sText As String
+        Dim i As Integer
+        Dim lenText As Integer
+        Dim Msg As String
+        Dim Word As String
+        Dim Asc As Integer
+        Dim Letter As String
+        If (Entry.Text = "" Or Entry.Text = "Please enter something.") Then
+            Entry.Text = "Please enter something."
+            Return
+        End If
+        sText = Entry.Text
+        lenText = Len(sText)
+        Dim Space As Integer
+        Space = InStr(1, sText, " ")
+        Dim Space2 As Integer
+        Space2 = 1
+        If GetChar(sText, lenText) <> " " Then
+            sText = sText + " "
+            lenText = Len(sText)
+        End If
+        While lenText <> Space
+            For i = Space2 To (Space - 1)
+                Word = Word + GetChar(sText, i)
+            Next i
+            Asc = Convert.ToInt32(Word)
+            Word = ""
+            Letter = Chr(Asc)
+            Msg = Msg + Letter
+            Space2 = Space + 1
+            Space = InStr(Space2, sText, " ")
+        End While
+        For i = Space2 To (Space - 1)
+            Word = Word + GetChar(sText, i)
+        Next i
+        Asc = Convert.ToInt32(Word)
+        Word = ""
+        Letter = Chr(Asc)
+        Msg = Msg + Letter
+        Dim fw As StreamWriter
+        Dim ReadString As String
+        Try
+            Dim s As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            fw = New StreamWriter(s & "\Text.txt", True)
+            ReadString = Msg
+            fw.WriteLine(ReadString)
+        Finally
+            fw.Close()
+        End Try
+    End Sub
+End Class
+
